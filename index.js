aca2f2a8947d8b9aae39bcbbce5a098f00a791ed
kaayoung <20190856@sungshin.ac.kr>
2022-07-10 17:10:36 +0900

    JS navbar contact scroll to page

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ document.addEventListener("scroll" , () => {
         navbar.classList.remove("navbar--color") ;
     }
 })
+
+// 스크롤 시 해당 페이지로 넘어가기 - 네비게이션 바 & Contact버튼
+const navbarMenu = document.querySelector(".navbar__menu");
+const homeContact = document.querySelector("#home .home__contact");
+
+function moveToPage(e) {
+    const target = e.target;
+    const link = target.dataset.link ;
+
+    if (!link) {
+        return;
+    } else {
+        const scrollTo = document.querySelector(link);
+        scrollTo.scrollIntoView({behavior:"smooth"});
+    }
+}
+
+navbarMenu.addEventListener("click" , moveToPage);
+homeContact.addEventListener("click", moveToPage);
+
+
+// 
